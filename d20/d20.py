27f7f4323642012315140327ca7ce907df36f4b4
Syed Muhammad Dawoud Sheraz Ali <dawoud.sheraz@gmail.com>
2024-08-20 22:00:44 +0500

    Day 20 part 1

---
+++ d20/d20.py
@@ -0,0 +1,157 @@
+
+from dataclasses import dataclass, field
+
+SAMPLE_0 = '''broadcaster -> a, b, c
+%a -> b
+%b -> c
+%c -> inv
+&inv -> a'''
+
+SAMPLE_1 = '''broadcaster -> a
+%a -> inv, con
+&inv -> b
+%b -> con
+&con -> output'''
+
+
+@dataclass
+class Module:
+    name: str
+    type: str | None
+    destination: list[str]
+    current_pulse: bool = False  # for %
+    low_pulse: int = 0
+    high_pulse: int = 0
+    states: dict[str, bool] = field(default_factory=dict)   # for &
+
+    def __init__(self, input_str):
+        name, destination = input_str.split('->')
+        name = name.strip()
+        destination = destination.strip()
+        if name[0] in '&%' or 'broadcaster' in name:
+            if 'broadcaster' in name:
+                self.name = name
+                self.type = 'broadcaster'
+            else:
+                self.name = name[1:]
+                self.type = name[0]
+        else:
+            self.name = name
+            self.type = None
+        destination = destination.split(', ')
+        self.destination = destination
+
+    def add_input(self, value):
+        self.states[value] = bool(0)
+
+    def process_pulse(self, pulse, sender):
+        if pulse:
+            self.high_pulse += 1
+        else:
+            self.low_pulse += 1
+        if self.type == '&':
+            self.states[sender] = pulse
+            if all(self.states.values()):
+                # self.low_pulse += 1
+                return 0, self.name
+            else:
+                # self.high_pulse += 1
+                return 1, self.name
+        elif self.type == '%':
+            if pulse:
+                return None, None
+            self.current_pulse = not self.current_pulse
+            if self.current_pulse:
+                # self.high_pulse += 1
+                return 1, self.name
+            else:
+                # self.low_pulse += 1
+                return 0, self.name
+        elif self.type == 'broadcaster':  # broadcast
+            if pulse:
+                # self.high_pulse += 1
+                return 1, self.name
+            else:
+                # self.low_pulse += 1
+                return 0, self.name
+        else:
+            return None, self.name
+
+
+def process_input(data: str):
+    module_dict = {}
+
+    for input_str in data.splitlines():
+        module = Module(input_str)
+        module.states = {}
+        module_dict[module.name] = module
+
+    # Add modules who do not sent to any other module
+    missing_modules = []
+    for name, module in module_dict.items():
+        for destination in module.destination:
+            if destination not in module_dict:
+                missing_modules.append(f"{destination} ->")
+    for module_str in missing_modules:
+        module = Module(module_str)
+        module.states = {}
+        module_dict[module.name] = module
+
+    for name, module in module_dict.items():
+        if name == 'broadcaster':
+            continue
+        for destination in module.destination:
+            if destination in module_dict:
+                module_dict[destination].add_input(name)
+    return module_dict
+
+
+def bfs(modules: dict[str, Module], start: str, pulse: bool):
+    queue = [(start, pulse, None)]
+
+    while queue:
+        current_module, pulse, sender = queue.pop(0)
+        if current_module not in modules:
+            continue
+        current_module_obj = modules[current_module]
+        sent_pulse, _ = current_module_obj.process_pulse(pulse, sender)
+        if sent_pulse is None:
+            continue
+        for destination in current_module_obj.destination:
+            # print(current_module, sent_pulse, destination)
+            queue.append((destination, sent_pulse, current_module))
+    return modules
+
+
+def part_1(data: str):
+    low, high = 0, 0
+    modules = process_input(data)
+    for _ in range(1000):
+        modules = bfs(modules, 'broadcaster', False)
+    for module in modules.values():
+        low += module.low_pulse
+        high += module.high_pulse
+    # print(modules)
+    print(low, high)
+    return low * high
+
+
+def part_2(data: str):
+    counter = 0
+    modules = process_input(data)
+    while True:
+        counter += 1
+        modules = bfs(modules, 'broadcaster', False)
+        print(counter, modules['rx'])
+
+        if modules['rx'].low_pulse == 1:
+            break
+
+    return counter
+
+
+with open('input.in', 'r') as f:
+    data = f.read()
+    # 949764474
+    # print(part_1(data))
+    print(part_2(data))
